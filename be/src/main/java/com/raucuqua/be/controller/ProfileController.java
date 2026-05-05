2a80440c6b0b8ce2e5cd312604dd55c231bfd31b
Bao Nguyen <22130021@st.hcmuaf.edu.vn>
2026-05-05 22:54:54 +0700

    done:  Danh sách Khách hàng từ Database (Profiles)

--- be/src/main/java/com/raucuqua/be/controller/ProfileController.java
+++ be/src/main/java/com/raucuqua/be/controller/ProfileController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,6 +17,11 @@ public class ProfileController {
 
     @Autowired
     private ProfileRepository profileRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Profile>> getAllProfiles() {
+        return ResponseEntity.ok(profileRepository.findAll());
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Profile> getProfile(@PathVariable UUID id) {
